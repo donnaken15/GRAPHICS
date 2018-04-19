ecf5775e0c4eb03e698f1bf23023a9d687b66307
donnaken15 <myminecraftfriend15@yahoo.com>
2018-04-19 12:47:06 -0400

    Graphics program for CMD

--- GRAPHICS/Properties/AssemblyInfo.cs
+++ GRAPHICS/Properties/AssemblyInfo.cs
@@ -1,36 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("GRAPHICS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Draw on Command Prompt")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("donnaken15")]
 [assembly: AssemblyProduct("GRAPHICS")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Wesley Kennedy 2018")]
+[assembly: AssemblyTrademark("Microsoft Corporation")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ee25c289-133b-426b-9478-7eb4976ed52b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
